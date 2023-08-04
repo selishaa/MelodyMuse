2919342594aa751c9a3efb9cf2fd2ee090778ed4
Selishaa <selishathapa5@gmail.com>
2023-08-04 01:29:31 -0400

    added song and time update

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ console.log("Welcome to Spotify");
 
 //Initialize the variables
 let songIndex = 0;
-let audioElement = new Audio('1.mp3');
-let masterPlay = document.getElementById('masterPlay')
-let MyProgressBar = document.getElementById('MyProgressBar')
-let gif = document.getElementById('gif')
+let audioElement = new Audio('song/1.mp3');
+let masterPlay = document.getElementById('masterPlay');
+let MyProgressBar = document.getElementById('MyProgressBar');
+let gif = document.getElementById('gif');
 let songs = [
     {songName: "Back to December", filepath:"song/1.mp3", coverPath: "covers/1.jpg"},
     {songName: "Back to December", filepath:"song/1.mp3", coverPath: "covers/1.jpg"},
@@ -21,15 +21,17 @@ let songs = [
 
 //Handle play/pause click
 masterPlay.addEventListener('click', ()=>{
-    if(audioElement.paused || audioElement.currentTime<=0){
+    if(audioElement.paused || audioElement.currentTime <= 0){
         audioElement.play();
         masterPlay.classList.remove('fa-circle-play');
+
         masterPlay.classList.add("fa-circle-pause");
         gif.style.opacity = 1;
     }
     else{
         audioElement.pause();
     masterPlay.classList.remove('fa-circle-pause');
+
     masterPlay.classList.add('fa-circle-play');
     gif.style.opacity = 0;
     }
@@ -38,7 +40,9 @@ masterPlay.addEventListener('click', ()=>{
 })
 
 //Listen to Events {}
-MyProgressBar.addEventListener('timeupdate', ()=>{
-console.log('timeupdate')
+audioElement.addEventListener('timeupdate', ()=>{
+console.log('timeupdate'); 
 //Update Seekbar
+progress = parseInt((audioElement.currentTime/audioElement.duration)*100);
+console.log('progress');
 } )
