247255631fa71db46774d97dc9fcb7fd1436c9eb
Selishaa <selishathapa5@gmail.com>
2023-08-04 00:30:23 -0400

    trying

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let songIndex = 0;
 let audioElement = new Audio('1.mp3');
 let masterPlay = document.getElementById('masterPlay')
 let MyProgressBar = document.getElementById('MyProgressBar')
+let gif = document.getElementById('gif')
 let songs = [
     {songName: "Back to December", filepath:"song/1.mp3", coverPath: "covers/1.jpg"},
     {songName: "Back to December", filepath:"song/1.mp3", coverPath: "covers/1.jpg"},
@@ -23,12 +24,14 @@ masterPlay.addEventListener('click', ()=>{
     if(audioElement.paused || audioElement.currentTime<=0){
         audioElement.play();
         masterPlay.classList.remove('fa-circle-play');
-        masterPlay.classList.add('fa-circle-pause');
+        masterPlay.classList.add("fa-circle-pause");
+        gif.style.opacity = 1;
     }
     else{
         audioElement.pause();
     masterPlay.classList.remove('fa-circle-pause');
     masterPlay.classList.add('fa-circle-play');
+    gif.style.opacity = 0;
     }
     
 
